ac69178d06da82c47cc99f5baab70c53e36f770f
Dennis de Greef <github@link0.net>
2015-03-08 17:14:32 +0100

    Initial commit

---
+++ src/ProfilerBundle/EventListener/ProfilingEventListener.php
@@ -0,0 +1,93 @@
+<?php
+
+/**
+ * ProfilingEventListener.php
+ *
+ * @author Dennis de Greef <github@link0.net>
+ */
+namespace Link0\ProfilerBundle\EventListener;
+
+use League\Flysystem\Adapter\Local;
+use Link0\Profiler\Filesystem;
+use Link0\Profiler\PersistenceHandler;
+use Link0\Profiler\Profiler;
+use Symfony\Component\Console\Event\ConsoleTerminateEvent;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\Console\Event\ConsoleCommandEvent;
+use Symfony\Component\HttpKernel\TerminableInterface;
+
+/**
+ * Class ProfilingEventListener
+ *
+ * Hooks into the symfony event system, and starts and stops the profiler on certain events
+ *
+ * Start:
+ *  - console.command
+ *  - kernel.request
+ * Stop:
+ *  - console.terminate
+ *  - kernel.terminate
+ *
+ * @package Link0\ProfilerBundle\EventListener
+ */
+class ProfilingEventListener
+{
+    /**
+     * A persistence handler should be fed, since null will disable persisting profiles
+     *
+     * @param PersistenceHandler $handler
+     */
+    public function __construct(PersistenceHandler $handler = null)
+    {
+        $this->profiler = new Profiler($handler);
+    }
+
+    /**
+     * @return Profiler
+     */
+    public function getProfiler()
+    {
+        return $this->profiler;
+    }
+
+    /**
+     * @event console.command
+     * @param ConsoleCommandEvent $event
+     */
+    public function onConsoleCommand(ConsoleCommandEvent $event)
+    {
+        $this->getProfiler()->start();
+    }
+
+    /**
+     * @event kernel.request
+     * @param GetResponseEvent $event
+     */
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        if (!$event->isMasterRequest()) {
+            return;
+        }
+
+        $this->getProfiler()->start();
+    }
+
+    /**
+     * @event console.terminate
+     * @param ConsoleTerminateEvent $event
+     */
+    public function onConsoleTerminate(ConsoleTerminateEvent $event)
+    {
+        $this->getProfiler()->stop();
+    }
+
+    /**
+     * @event kernel.terminate
+     * @param TerminableInterface $event
+     */
+    public function onKernelTerminate(TerminableInterface $event)
+    {
+        $this->getProfiler()->stop();
+    }
+
+}
---
+++ src/ProfilerBundle/Link0ProfilerBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Link0\ProfilerBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class Link0ProfilerBundle extends Bundle
+{
+}
